38adaf990c76d286b69996497a5bc585170ced02
Reyes, Karl Siegfred L <112219823+KarlSiegfredReyes@users.noreply.github.com>
2022-09-17 10:26:23 +0800

    Create Asynchronus.py

---
+++ Asynchronus.py
@@ -0,0 +1,6 @@
+Salary_rate = 600 #per day
+Salary = Salary_rate * 15 #15 days
+Taxable_amount = Salary*0.30 #Taxation is theft
+Net_pay = Salary - Taxable_amount
+
+print(f'The Net pay is {Net_pay}')
